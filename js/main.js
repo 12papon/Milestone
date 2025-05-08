7e98334c526b474e524a7f3563d5acbd63189b0d
papon khan <paponk25@gmail.com>
2025-05-08 22:42:19 +0600

    new exam

--- js/main.js
+++ js/main.js
@@ -1,52 +1,31 @@
+const mainData = JSON.parse(data).data;
+console.log(mainData);
 
-const milestoneData = JSON.parse(data).data;
-console.log(milestoneData);
-
-function loadData(){
-    const mainElement = document.querySelector('.milestone');
-
-    mainElement.innerHTML = `${milestoneData.map(
-        function(value){
-          return  `<div class="item">
+    function mileStone(){
+        const mileStoneList = document.querySelector('.milestone');
+        mileStoneList.innerHTML = `${mainData.map(function(value){
+            return `<div class="item">
                     <!-- milstine -->
                     <div class="list">
                         <div><input type="checkbox" name="item"></div>
-                        <p onclick='show(this, ${value._id})'>
+                        <p>
                             ${value.name}
                             <i class="fa-solid fa-angle-down"></i>
                         </p>
-                    </div>
+                    </div> 
                     <!-- Hidden item -->
                     <div class="hidden-item">
-                        ${value.modules.map(function(module){
-                        return `<div class="sub-item">
-                                        <p style="text-align: left;">${module.name}</p>
-                                 </div>`;
-                        }).join("")}
+                        <div class="sub-item">
+                                ${value.modules.map(function(value){
+                                    return `   <p> ${value.name}  </p>`
+                                }).join("")}
+                        </div>
                     </div>
                 </div>`
-        }
-    ).join("")}`
-}
-loadData();
+        }).join("")}`
+    }
+    mileStone();
 
-    function show(element, id){
-        const parent = element.parentElement;
-        const sibling = parent.nextElementSibling;
-        const show  = document.querySelector('.show');
-        if(!sibling.classList.contains('show') && show)
-        show.classList.remove('show')
-        sibling.classList.toggle('show'); 
-        shown(id);
-    }
-    function shown(id){
-        const thumbnail = document.querySelector('.thumbnailImage');
-        const title = document.querySelector('.title');
-        const Discriptopn = document.querySelector('.Discriptopn');
-        thumbnail.src = milestoneData[id].image;
-        title.innerHTML = milestoneData[id].name;
-        Discriptopn.innerHTML = milestoneData[id].description;
-    }
 
 const nnn = `<div class="item">
                     <!-- milstine -->
@@ -66,3 +45,70 @@ const nnn = `<div class="item">
                         </div>
                     </div>
                 </div>`
+// const milestoneData = JSON.parse(data).data;
+// console.log(milestoneData);
+
+// function loadData(){
+//     const mainElement = document.querySelector('.milestone');
+
+//     mainElement.innerHTML = `${milestoneData.map(
+//         function(value){
+//           return  `<div class="item" id='${value._id}'>
+//                     <!-- milstine -->
+//                     <div class="list">
+//                         <div><input onclick="done(this, ${value._id})" type="checkbox" name="item"></div>
+//                         <p onclick='show(this, ${value._id})'>
+//                             ${value.name}
+//                             <i class="fa-solid fa-angle-down"></i>
+//                         </p>
+//                     </div>
+//                     <!-- Hidden item -->
+//                     <div class="hidden-item">
+//                         ${value.modules.map(function(module){
+//                         return `<div class="sub-item">
+//                                         <p style="text-align: left;">${module.name}</p>
+//                                  </div>`;
+//                         }).join("")}
+//                     </div>
+//                 </div>`
+//         }
+//     ).join("")}`
+// }
+// loadData();
+
+//     function show(element, id){
+//         const parent = element.parentElement;
+//         const sibling = parent.nextElementSibling;
+//         const show  = document.querySelector('.show');
+//         if(!sibling.classList.contains('show') && show)
+//         show.classList.remove('show')
+//         sibling.classList.toggle('show'); 
+//         shown(id);
+//     }
+//     function shown(id){
+//         const thumbnail = document.querySelector('.thumbnailImage');
+//         const title = document.querySelector('.title');
+//         const Discriptopn = document.querySelector('.Discriptopn');
+//         thumbnail.style.opacity = "0"
+//         thumbnail.src = milestoneData[id].image;
+//         title.innerHTML = milestoneData[id].name;
+//         Discriptopn.innerHTML = milestoneData[id].description;
+//     }
+//     const thumbnail = document.querySelector('.thumbnailImage');
+//         thumbnail.onload = function(){
+//         thumbnail.style.opacity = "1";
+//     }
+
+//     function done(checkbox, id){
+//         const singleMileStone = document.querySelector('.item');
+//         const doneList = document.querySelector('.doneList');
+//         const item = document.getElementById(id);
+//         console.log(item);
+//         if(checkbox.checked){
+//             singleMileStone.remove(id);
+//             doneList.appendChild(item);
+//         }else{
+
+//         }
+        
+//     }
