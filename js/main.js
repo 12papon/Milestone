d0eb6e1cc3595f693612f9febd741f2e2879acce
papon khan <paponk25@gmail.com>
2025-05-09 09:47:33 +0600

    Some change

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 const mainData = JSON.parse(data).data;
-console.log(mainData);
 
     function mileStone(){
         const mileStoneList = document.querySelector('.milestone');
         mileStoneList.innerHTML = `${mainData.map(function(value){
-            return `<div class="item">
+            return `<div class="item" id='${value._id}'>
                     <!-- milstine -->
                     <div class="list">
-                        <div><input type="checkbox" name="item"></div>
-                        <p>
+                        <div><input type="checkbox" name="item" onclick="changlist(this, ${value._id})"></div>
+                        <p  onclick="shown(this, ${value._id})">
                             ${value.name}
                             <i class="fa-solid fa-angle-down"></i>
                         </p>
@@ -17,7 +16,7 @@ console.log(mainData);
                     <div class="hidden-item">
                         <div class="sub-item">
                                 ${value.modules.map(function(value){
-                                    return `   <p> ${value.name}  </p>`
+                                    return `<p> ${value.name}</p>`
                                 }).join("")}
                         </div>
                     </div>
@@ -25,6 +24,56 @@ console.log(mainData);
         }).join("")}`
     }
     mileStone();
+
+    function shown(element, id){
+        const hiddenElement = element.parentElement.nextElementSibling;
+        const show = document.querySelector('.show');
+        const thumbnail = document.querySelector('.thumbnailImage');
+        if(!hiddenElement.classList.contains('show') && show){
+            show.classList.remove('show');
+        }
+        hiddenElement.classList.toggle('show');  
+        imageHandaling(id); 
+    }
+
+    function imageHandaling(id){
+        const thumbnail = document.querySelector('.thumbnailImage');
+        const title = document.querySelector('.title');
+        const Discriptopn = document.querySelector('.Discriptopn');
+        thumbnail.src = mainData[id].image;
+        thumbnail.style.opacity = '0';
+        title.innerText = mainData[id].name;
+        Discriptopn.innerText = mainData[id].description;
+    }
+    document.querySelector('.thumbnailImage').onload = function(){
+        this.style.opacity = '1';   
+    };
+        
+
+    function changlist(check, id){
+        const milestone = document.querySelector('.milestone');
+        const singleMileStone = document.querySelector('.item');
+        const doneList = document.querySelector('.doneList');
+        const item = document.getElementById(id);
+                console.log(item);
+                if(check.checked){
+                    milestone.removeChild(item);
+                    doneList.appendChild(item);
+                    // singleMileStone.remove(id);
+                    // doneList.appendChild(item);
+                }else{
+                    item.remove();
+                    milestone.appendChild(item)
+                                        // milestone.appendChild(item);
+                    // doneList.remove(item);
+                }
+        // }else{
+        //     milestone.appendChild(currentItem);
+        //     doneList.remove(currentItem);
+        // }
+    }
+
+
 
 
 const nnn = `<div class="item">
